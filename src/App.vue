3d11e21f175aee730e06a014e8a8060a6288ac24
ShadyHippo <tim.vandyke123@gmail.com>
2019-04-06 18:26:23 -0400

    finished home page

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,69 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">Assignment 0</router-link>
+      <router-link to="/about">Assignment 1</router-link>
     </div>
+    
     <router-view/>
   </div>
 </template>
 
 <style>
+.indentHeading {
+  padding-left: 35px;
+}
+
+.left {
+  text-align: 'left';
+}
+
+.picture {
+  padding: 8px;
+  padding-right: 45px;
+  padding-left: 30px;
+}
+
+.body {
+  font-family: 'Arial';
+  font-size: 14px;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+
 #app {
+  margin-left: 160px; 
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
+
 #nav {
-  padding: 30px;
+  width: 150px;
+  position: fixed;
+  z-index: 1;
+  top: 20px;
+  left: 10px;
+  background: #eee;
+  overflow-x: hidden;
+  padding: 8px 0;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  padding: 6px 8px 6px 8px;
+  text-decoration: none;
+  font-size: 20px;
+  color: #2196F3;
+  display: block;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #26adc5;
+}
+
+#nav a:hover {
+  color: #064579;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <Outline msg="Welcome to Your Vue.js App"/>
-    <Outline msg="two?"/>
+  <div>
+    <img alt="don't skip the tutorial" src="../assets/MainPageMeme.jpg" align='right' class="picture">
+    <h1 class="indentHeading">The Beginning</h1>
+    <h2 class="indentHeading">Now what? </h2>
+    <p class="body">
+      Ultimately, the goal is for you to learn something her. I know this might seem crazy, but Java is actually kind of a game to me. I find it to be a lot of fun, and making something is a very satisfying experience. 
+    </p>
+    <p class="body">
+      Below is going to be a general outline for each lesson, and how to read what I'm going for. 
+    </p>
+    <h2 class="indentHeading">Estimated Time:</h2>
+    <p class="body">Estimated Time (beginner): Estimated time for someone who has no experience with the tools and technologies being used to complete the task, I am just guessing for these. Don't take offense at how long or short they are, I don't actually know. It will generally be much longer than the estimated time (me). </p>
+    <p class="body">Estimated Time (me): This is my estimate for how long it would take someone to build the assignment with the level of experience I have in these technologies. Generally this will be far shorter than the above. </p>
+    <p class="body">I include both in order to prevent you from spending hours and hours and hours barking up the wrong tree. For example, assignment 1 estimated time (beginner) is literally 30 or more times longer than mine. This isn't to make you feel bad, but it's to say "if it sounds easy and you think you figured it out in 4 minutes, you probably did." Also, if you spend longer than my time estimate on it, that probably means at some point you've stopped making progress and don't know how to google the problems yet. </p>
+    <p class="body">That is the biggest skill college has taught me for CS, so I think it's important to have you spend some time doing it as well (as I know you will) instead of me giving you specific resources to use.</p>
+    <p class="body">However, after a certain point it's time to say "this is a waste of time I'm not making progress", and ask me a question or two. I generally tend to be more thorough than needed, and if I start rambling tell me lol. </p>
+    <h2 class="indentHeading">IMPORTANT</h2>
+    <p class="body">Says what concepts we are covering and what the goal of the assignment is. </p>
+    <h2 class="indentHeading">Assignment: </h2>
+    <ol class="body">
+      <li>The actual assignment begins here</li>
+      <li>I will give different steps. I can make these as vague or detailed as you would like as time goes on, we will figure this out better.</li>
+    </ol>
+    <h2 class="indentHeading">Helpful Hints: </h2>
+    <p class="body">Helpful Hints: If I think there is something that is important for you to know that didn't fit somewhere else it will go here. This could range anywhere from code snippets, documentation links, to a meme about the task at hand. </p>
+    <p class="body">I tend to look at things How I look at them, and will attempt to explain things different ways, but I may not be able to explain in a way you understand it. Google is your best friend! </p>
+    <img alt="explain meme" src="../assets/Outline/OutlineMeme.jpg" align='center' class="picture">
   </div>
 </template>
 
